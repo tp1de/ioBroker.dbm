0352d9349c41d66e3603766bde81e1579e41cfd6
tp1de <tp1degit@gmail.com>
2022-01-22 16:45:40 +0100

    test

--- admin/words.js
+++ admin/words.js
@@ -14,28 +14,9 @@ systemDictionary = {
 		"pl": "Ustawienia adaptera dla dbm",
 		"zh-cn": "dbm的适配器设置"
 	},
-	"option1": {
-		"en": "option1",
-		"de": "Option 1",
-		"ru": "Опция 1",
-		"pt": "Opção 1",
-		"nl": "Optie 1",
-		"fr": "Option 1",
-		"it": "opzione 1",
-		"es": "Opción 1",
-		"pl": "opcja 1",
-		"zh-cn": "选项1"
+	"db": {
+		"en": "database",
+		"de": "Datenbank",
 	},
-	"option2": {
-		"en": "option2",
-		"de": "Option 2",
-		"ru": "вариант 2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
-	}
+
 };
